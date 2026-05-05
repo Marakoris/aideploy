03727ca1d6f9e53722f5a2f4306d23e08118cb09
Maksim Zhelezkin <marakoris@gmail.com>
2026-05-05 11:42:01 +0000

    Replace demo chatbot with aideploy.ru widget script

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ async function registerRoutes(app2) {
     port: 587,
     secure: false,
     auth: {
-      user: process.env.EMAIL_USER || "info@aideploy.ru",
+      user: process.env.EMAIL_USER || "",
       pass: process.env.EMAIL_PASS || ""
     },
     tls: {
