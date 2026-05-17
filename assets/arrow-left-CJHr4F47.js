96bc88ea23cebcaf943d67a0cce49b3c8584ccb9
Maksim Zhelezkin <marakoris@gmail.com>
2026-05-17 16:04:10 +0000

    feat: replace form with interactive quiz for better conversion

--- assets/arrow-left-CJHr4F47.js
+++ assets/arrow-left-CJHr4F47.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CZPG1VO6.js";/**
+import{c as e}from"./index-BoQuPOYx.js";/**
  * @license lucide-react v0.453.0 - ISC
  *
  * This source code is licensed under the ISC license.
